bf795efdb42c5e4f25cb77c7c7e13517a6b6fad9
Yongjun Ou <ou.yo@husky.neu.edu>
2020-04-11 19:30:26 -0400

    1.csv data relocate 2.add unit test

---
+++ Daycare_ID_6201/test/edu/neu/csye6200/CSVdataTest.java
@@ -0,0 +1,15 @@
+package edu.neu.csye6200;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CSVdataTest {
+
+	@Test
+	void test() {
+
+		System.out.println(CSVdata.readTeacherData());
+	}
+
+}
