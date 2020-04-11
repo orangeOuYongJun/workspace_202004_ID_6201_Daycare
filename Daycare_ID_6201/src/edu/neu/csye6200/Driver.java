c75fd4ca6de08479b9285b07cea053c7254074ca
mengzhene <594306847@qq.com>
2020-04-10 21:40:21 -0400

    I update something useless

--- Daycare_ID_6201/src/edu/neu/csye6200/Driver.java
+++ Daycare_ID_6201/src/edu/neu/csye6200/Driver.java
@@ -5,6 +5,7 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("system runs....");
+		System.out.println("zmz is here");
 	}
 
 }
