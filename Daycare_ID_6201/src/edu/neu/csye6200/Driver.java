77a3310ce768917f0e28b225912fbd393e16cb04
WeisiminPeng <59750029+WeisiminPeng@users.noreply.github.com>
2020-04-11 18:47:40 -0400

    a

--- Daycare_ID_6201/src/edu/neu/csye6200/Driver.java
+++ Daycare_ID_6201/src/edu/neu/csye6200/Driver.java
@@ -5,10 +5,10 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("system runs....");
+// 		System.out.println("system runs....");
 
-//		new secondui();
-		System.out.println("system done....");
+// //		new secondui();
+// 		System.out.println("system done....");
 		// System.out.println("system runs....");
 		// new secondui();
 		// System.out.println("system done....");
