058566093b7f73342be4609b2753cf474a5616cc
Yongjun Ou <ou.yo@husky.neu.edu>
2020-04-11 21:14:58 -0400

    1. resize code

--- Daycare_ID_6201/src/SwingUI/ClassroomUI.java
+++ Daycare_ID_6201/src/SwingUI/ClassroomUI.java
@@ -1,6 +1,6 @@
 package SwingUI;
 
-import javax.swing.*; 
+import javax.swing.*;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import javax.swing.JFrame;
@@ -8,59 +8,50 @@ import java.awt.*;
 
 public class ClassroomUI {
 
-
-		public ClassroomUI(){
+	public ClassroomUI() {
 		JFrame jf = new JFrame("Classroom Situation");
-		
-		jf.setSize(400, 300);	
-	    jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-	    jf.setLocationRelativeTo(null);
-	    
-	    GridLayout layout = new GridLayout(3, 3);
-	    
 
-	JPanel panel = new JPanel(layout);
+		jf.setSize(400, 300);
+		jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		jf.setLocationRelativeTo(null);
 
-	
-	JLabel lab01 = new JLabel("Year:");
-	JButton btn03 = new JButton("Classroom");
-	JButton btn04 = new JButton("Teacher");
-	JButton btn05 = new JButton("Student");
-	;
+		GridLayout layout = new GridLayout(3, 3);
 
-	JButton btn06 = new JButton("Immunization Record");
+		JPanel panel = new JPanel(layout);
 
+		JLabel lab01 = new JLabel("Year:");
+		JButton btn03 = new JButton("Classroom");
+		JButton btn04 = new JButton("Teacher");
+		JButton btn05 = new JButton("Student");
+		;
 
-	String[] listYear = new String[]{"2018", "2019", "2020", "2021"};
-	
-	final JComboBox<String> comboBox = new JComboBox<String>(listYear);
+		JButton btn06 = new JButton("Immunization Record");
 
-	
-	comboBox.addItemListener(new ItemListener() {
-	    @Override
-	    public void itemStateChanged(ItemEvent e) {
-	       
-	        if (e.getStateChange() == ItemEvent.SELECTED) {
-	            System.out.println("ѡ��: " + comboBox.getSelectedIndex() + " = " + comboBox.getSelectedItem());
-	        }
-	    }
-	});
-	
-	comboBox.setSelectedIndex(2);
+		String[] listYear = new String[] { "2018", "2019", "2020", "2021" };
 
+		final JComboBox<String> comboBox = new JComboBox<String>(listYear);
 
-	panel.add(lab01);
-	panel.add(comboBox);
-	panel.add(btn03);
-	panel.add(btn04);
-	panel.add(btn05);
-	panel.add(btn06);
+		comboBox.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent e) {
 
-	jf.setContentPane(panel);
-	jf.setVisible(true); 
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+					System.out.println("ѡ��: " + comboBox.getSelectedIndex() + " = " + comboBox.getSelectedItem());
+				}
+			}
+		});
 
-		}
+		comboBox.setSelectedIndex(2);
+
+		panel.add(lab01);
+		panel.add(comboBox);
+		panel.add(btn03);
+		panel.add(btn04);
+		panel.add(btn05);
+		panel.add(btn06);
+
+		jf.setContentPane(panel);
+		jf.setVisible(true);
+
 	}
-
-
-
+}
--- Daycare_ID_6201/src/SwingUI/ImmunizationSituationUI.java
+++ Daycare_ID_6201/src/SwingUI/ImmunizationSituationUI.java
@@ -18,12 +18,10 @@ import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
 
 public class ImmunizationSituationUI {
-	
 
 	public static void main(String[] agrs) {
 		JFrame frame = new JFrame("ImmunizationSituationTable");
-       
-       
+
 		frame.setBounds(0, 0, 800, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container contentPane = frame.getContentPane();
@@ -36,7 +34,7 @@ public class ImmunizationSituationUI {
 			}
 		}
 		String[] name = { "stuId", "name", "classroomID", "teacherID", "age", "immunizationdates", "overdue(T/F)" };
-	
+
 		JScrollPane scrollPane = new JScrollPane();
 		JTable table = new JTable(tableDate, name);
 		table.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
@@ -44,15 +42,12 @@ public class ImmunizationSituationUI {
 		jScrollPane.setBounds(0, 0, 800, 350);
 		contentPane.add(jScrollPane);
 
-
-	
 		JButton button = new JButton("One-click vaccination");
-	    button.setLayout(null);
-	    button.setBounds(350, 350, 200, 50);
-	    button.setBackground(Color.RED);
-	    contentPane.add(button);
+		button.setLayout(null);
+		button.setBounds(350, 350, 200, 50);
+		button.setBackground(Color.RED);
+		contentPane.add(button);
 
-		
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				CurrentSituation homepage = new CurrentSituation();
@@ -61,7 +56,7 @@ public class ImmunizationSituationUI {
 			}
 		});
 
-		 frame.setLayout(null);//cancel the default layout
+		frame.setLayout(null);// cancel the default layout
 		frame.setVisible(true);
 
 	}
